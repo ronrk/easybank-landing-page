c76adce2bc44bef356a5cca7af204bf05063d108
ronrk <ronrk11@gmail.com>
2022-11-28 23:23:14 +0200

    finish project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  Navbar,
+  HeaderHero,
+  Features,
+  Articles,
+  Footer,
+  MobileMenu,
+} from "./Components";
 
-function App() {
+const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenuHandler = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar toggleMenu={toggleMenuHandler} showMenu={isMenuOpen} />
+      <MobileMenu showMenu={isMenuOpen} />
+      <main
+        className={isMenuOpen ? "app-container overlay" : "app-container"}
+        onClick={closeMenu}
+      >
+        <HeaderHero />
+        <Features />
+        <Articles />
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
